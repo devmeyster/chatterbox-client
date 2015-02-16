e8088aa30f9320f7bb8133df4d5f4d40d7922f32
devmeyster <arthurfbi@yahoo.com>
2015-02-16 15:09:10 -0800

    Complete requirement to display messages from server

--- client/scripts/app.js
+++ client/scripts/app.js
@@ -1,2 +1,39 @@
-// YOUR CODE HERE:
+// app.js
 
+// window.app = {};
+
+var App = function(){
+  this.server = 'https://api.parse.com/1/classes/chatterbox';
+
+};
+
+App.prototype.init = function(){
+
+};
+
+App.prototype.fetch = function(){
+
+  $.ajax({
+    // always use this url
+    url: this.server,
+    type: 'GET',
+    //data: JSON.stringify(message),
+    contentType: 'application/json',
+    success: function (data) {
+
+      _.each(data.results, function(item){
+        $('#main').append('<div>'+item.username +" : "+ item.text );
+      });
+
+    },
+    error: function (data) {
+      // see: https://developer.mozilla.org/en-US/docs/Web/API/console.error
+      console.error('chatterbox: Failed to send message');
+    }
+  });
+};
+
+
+var app = new App();
+
+app.fetch();
