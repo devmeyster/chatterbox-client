bb41f273e33ca3dc5d9f5f99a59dced2a43b7fbe
devmeyster <arthurfbi@yahoo.com>
2015-02-16 15:39:52 -0800

    Add refresh functionality and escape user input

--- client/scripts/app.js
+++ client/scripts/app.js
@@ -4,30 +4,36 @@
 
 var App = function(){
   this.server = 'https://api.parse.com/1/classes/chatterbox';
-
+  this.init();
 };
 
 App.prototype.init = function(){
-
+  var self = this;
+  self.fetch();
+  setInterval(function(){self.fetch();}, 3000);
 };
 
 App.prototype.fetch = function(){
 
   $.ajax({
-    // always use this url
     url: this.server,
     type: 'GET',
-    //data: JSON.stringify(message),
+    // data: JSON.stringify({limit: 1000}),
     contentType: 'application/json',
     success: function (data) {
-
+      $('.messages div').remove();
       _.each(data.results, function(item){
-        $('#main').append('<div>'+item.username +" : "+ item.text );
+        var $usr = $('<div></div>');
+        var $msg = $('<div></div>');
+
+        $usr.text(item.username);
+        $msg.text(item.text);
+
+        $('.messages').append('<div>' + $usr.html() + ' : ' + $msg.html() + '</div>');
       });
 
     },
     error: function (data) {
-      // see: https://developer.mozilla.org/en-US/docs/Web/API/console.error
       console.error('chatterbox: Failed to send message');
     }
   });
@@ -36,4 +42,4 @@ App.prototype.fetch = function(){
 
 var app = new App();
 
-app.fetch();
+
