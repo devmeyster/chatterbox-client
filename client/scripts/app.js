5771a398c8e6e5bd532a4966788fe259cbb34e66
devmeyster <arthurfbi@yahoo.com>
2015-02-16 16:33:07 -0800

    Allow users to select username send messages

--- client/scripts/app.js
+++ client/scripts/app.js
@@ -5,12 +5,39 @@
 var App = function(){
   this.server = 'https://api.parse.com/1/classes/chatterbox';
   this.init();
+  this.name = location.search.split("=")[1];
 };
 
 App.prototype.init = function(){
   var self = this;
   self.fetch();
   setInterval(function(){self.fetch();}, 3000);
+
+  $('.send-input').on('click', function(){
+    var input = $('.user-input').val();
+    $('.user-input').val('');
+    var msg = {
+      username: this.name,
+      text: input
+    };
+    this.send(msg);
+  }.bind(this));
+};
+
+App.prototype.send = function(msg){
+  $.ajax({
+    url: this.server,
+    type: 'POST',
+    data: JSON.stringify(msg),
+    contentType: 'application/json',
+    success: function (data) {
+      console.log(data);
+    },
+    error: function (data) {
+      console.error('chatterbox: Failed to send message');
+    }
+  });
+
 };
 
 App.prototype.fetch = function(){
@@ -18,7 +45,9 @@ App.prototype.fetch = function(){
   $.ajax({
     url: this.server,
     type: 'GET',
-    // data: JSON.stringify({limit: 1000}),
+    data: {
+      limit: 100
+    },
     contentType: 'application/json',
     success: function (data) {
       $('.messages div').remove();
@@ -31,7 +60,6 @@ App.prototype.fetch = function(){
 
         $('.messages').append('<div>' + $usr.html() + ' : ' + $msg.html() + '</div>');
       });
-
     },
     error: function (data) {
       console.error('chatterbox: Failed to send message');
@@ -41,5 +69,3 @@ App.prototype.fetch = function(){
 
 
 var app = new App();
-
-
